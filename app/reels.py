3d5e9a742d7e844c217c22f12f6fc82ef3ef5c8b
ahmedelasmai <ahmed.elasmai@outlook.com>
2024-04-28 11:52:49 +0100

    search feature for reels and profiles

--- app/reels.py
+++ app/reels.py
@@ -1,24 +1,36 @@
 import requests  
 
-headers = {
-    'Authorization': 'AzTxNqak0lM0K8DYZvTwHOirmCF9vRD4URwMNhizm6kShLiqdEd1ii2F'
-}
 
-response = requests.get('https://api.pexels.com/videos/popular', headers=headers)  
+class Reels:
+    
+    def get_video(self, search):
+        
+        links = []
 
-if response.status_code == 200:
-    data = response.json()
-    videos = data['videos']
-    
-    
-    links = []
-    
-    for video in videos:
-        video_files = video['video_files']
-        for file in video_files:
-            link = file['link']
-            links.append(link)  
-    
-    
-else:
-    print('error', response.status_code)
+        headers = {
+            'Authorization': 'AzTxNqak0lM0K8DYZvTwHOirmCF9vRD4URwMNhizm6kShLiqdEd1ii2F'
+        }
+
+        params = {
+            "query" : f"{search}"   
+        }
+
+        response = requests.get('https://api.pexels.com/videos/search', params=params, headers=headers)  
+
+        if response.status_code == 200:
+            data = response.json()
+            videos = data['videos']
+            
+            for link in videos:
+                links.append(link['video_files'][0]['link'])
+                
+        else:
+            print('error', response.status_code)    
+
+        return links
+
+
+
+
+
+
--- app/routes.py
+++ app/routes.py
@@ -1,27 +1,47 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app
-from app.reels import links
-profiles = 'james'   #  FOR TESTING
+from app.reels import Reels
 
-@app.route('/')
+profiles = 'james'   #  FOR TESTIN
+video_index = 0
+
+reel = Reels()
+
+@app.route('/')      
 @app.route('/index', methods = ['GET', 'POST'])
 def index():
-    print(profiles)
+    global search
     if request.method == 'POST':
         search = request.form['search']
-        
+        if search[0] == '@':
+            search = search[1:]
         if profiles == search:
             return redirect(url_for('profile')) 
-    
+        
+        return redirect(url_for('reels'))    
     return render_template('index.html')
 
 @app.route('/profile')
 def profile():
     return render_template('profile.html', profiles=profiles)
 
-@app.route('/reels')
+@app.route('/reels', methods = ['GET', 'POST'])
 def reels():
-    return render_template('reels.html',links=links)
+    links = reel.get_video(search)
+    global video_index
+
+    if request.method == 'POST':    
+        if 'next' in request.form and video_index < len(links):
+            video_index += 1
+            link = links[video_index]
+            return render_template('reels.html', reel=link)
+        
+        if 'back' in request.form and video_index > 1:
+            video_index -= 1
+            link = links[video_index]
+            return render_template('reels.html', reel=link)
+    link = links[0]
+    return render_template('reels.html', reel=link)
 
 
 
